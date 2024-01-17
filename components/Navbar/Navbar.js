084ca5501db30960bd83db2cb4ab034112fd7ff1
anikahammedkhan <anikkhan1105@gmail.com>
2024-01-17 23:26:44 +0600

    navbar updated

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -1,112 +1,173 @@
 import React, { useState } from 'react'
-import Image from 'next/image'
-import Logo from './../../Assets/images/logo.png'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 
 const Navbar = () => {
   const pathname = usePathname()
+  const [activePage, setActivePage] = useState(pathname)
+
+  const handlePageChange = (page) => {
+    setActivePage(page)
+  }
 
   const menu = (
     <>
-      <li className='flex justify-center'>
+      <li
+        className={`h-3/4 flex items-center justify-center relative ${
+          pathname === '/' ? 'active' : ''
+        }`}
+        onClick={() => handlePageChange('/')}
+      >
         <Link
-          className={`link nav-link ${pathname === '/' ? 'active' : ''}`}
+          className='flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition'
           href='/'
         >
           Home
         </Link>
+        {pathname === '/' ? (
+          <span className='bg-gray-100 rounded-full absolute inset-0 -z-10'></span>
+        ) : (
+          <></>
+        )}
       </li>
-      <li className='flex justify-center'>
+      <li
+        className={`h-3/4 flex items-center justify-center relative ${
+          pathname === '/projects' ? 'active' : ''
+        }`}
+        onClick={() => handlePageChange('/projects')}
+      >
         <Link
-          className={`link nav-link ${
-            pathname === '/projects' ? 'active' : ''
-          }`}
+          className='flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition'
           href='/projects'
         >
           Projects
         </Link>
+        {pathname === '/projects' ? (
+          <span
+            className='bg-gray-100 rounded-full absolute inset-0 -z-10'
+            style={{
+              transform: 'none',
+              transformOrigin: '50% 50% 0px',
+            }}
+          ></span>
+        ) : (
+          <></>
+        )}
       </li>
-      <li className='flex justify-center'>
+      <li
+        className='h-3/4 flex items-center justify-center relative'
+        style={{
+          opacity: '1',
+          transform: 'none',
+        }}
+      >
         <Link
-          className={`link nav-link ${pathname === '/about' ? ' active' : ''}`}
+          className='flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition'
           href='/about'
         >
           About
         </Link>
+        {pathname === '/about' ? (
+          <span
+            className='bg-gray-100 rounded-full absolute inset-0 -z-10'
+            style={{
+              transform: 'none',
+              transformOrigin: '50% 50% 0px',
+            }}
+          ></span>
+        ) : (
+          <></>
+        )}
       </li>
-      <li className='flex justify-center'>
+      <li
+        className='h-3/4 flex items-center justify-center relative'
+        style={{
+          opacity: '1',
+          transform: 'none',
+        }}
+      >
         <Link
-          className={`link nav-link ${pathname === '/contact' ? 'active' : ''}`}
+          className='flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition'
           href='/contact'
         >
           Contact
         </Link>
+        {pathname === '/contact' ? (
+          <span
+            className='bg-gray-100 rounded-full absolute inset-0 -z-10'
+            style={{
+              transform: 'none',
+              transformOrigin: '50% 50% 0px',
+            }}
+          ></span>
+        ) : (
+          <></>
+        )}
       </li>
-      <li className='flex justify-center'>
+      <li
+        className='h-3/4 flex items-center justify-center relative'
+        style={{
+          opacity: '1',
+          transform: 'none',
+        }}
+      >
         <Link
-          className={`link nav-link ${pathname === '/blogs' ? 'active' : ''}`}
+          className='flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition'
           href='/blogs'
         >
           Blogs
+        </Link>
+        {pathname === '/blogs' ? (
+          <span
+            className='bg-gray-100 rounded-full absolute inset-0 -z-10'
+            style={{
+              transform: 'none',
+              transformOrigin: '50% 50% 0px',
+            }}
+          ></span>
+        ) : (
+          <></>
+        )}
+      </li>
+      <li
+        className='h-3/4 flex items-center justify-center relative'
+        style={{
+          opacity: '1',
+          transform: 'none',
+        }}
+      >
+        <Link
+          className='flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition'
+          href='/req-project'
+        >
+          Request Project
         </Link>
       </li>
     </>
   )
 
   return (
-    <div className='drawer lg:h-[80px] sticky top-0 bg-white z-[99]'>
-      <input id='my-drawer-3' type='checkbox' className='drawer-toggle' />
-      <div className='drawer-content flex flex-col bg-[#eeebeb]'>
-        <div className='max-w-[1440px] mx-auto navbar relative flex justify-between px-5'>
-          <Link href='/'>
-            <Image
-              src={Logo}
-              alt='logo'
-              className='lg:w-[200px] md:w-[250px] w-[150px]'
-            />
-          </Link>
-          <div className='flex-none lg:hidden'>
-            <label
-              htmlFor='my-drawer-3'
-              aria-label='open sidebar'
-              className='btn btn-square btn-ghost'
-            >
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                fill='none'
-                viewBox='0 0 24 24'
-                className='inline-block w-6 h-6 stroke-current text-black'
-              >
-                <path
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                  strokeWidth='2'
-                  d='M4 6h16M4 12h16M4 18h16'
-                ></path>
-              </svg>
-            </label>
-          </div>
-          <div className='flex-none hidden lg:block'>
-            <ul className='flex gap-5 w-full text-[16px] font-semibold z-20 text-white hover:text-white'>
-              {menu}
-            </ul>
-          </div>
-        </div>
-      </div>
-      <div className='drawer-side'>
-        <label
-          htmlFor='my-drawer-3'
-          aria-label='close sidebar'
-          className='drawer-overlay'
-        ></label>
-        <ul className='flex flex-col pt-10 w-[200px] min-h-full text-white font-semibold bg-gray-300'>
-          <Link href='/'>
-            <Image src={Logo} alt='logo' className='w-[180px] ps-10' />
-          </Link>
+    <div
+      className='fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.1] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[37rem] sm:rounded-full z-[999]'
+      style={{
+        opacity: '1',
+        transform: 'translateX(-50%) translateY(0px) translateZ(0px)',
+      }}
+    >
+      <nav className='flex fixed left-1/2 h-12 -translate-x-1/2 -translate-y-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0'>
+        <ul className='flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[38rem] sm:flex-nowrap sm:gap-5'>
           {menu}
         </ul>
-      </div>
+      </nav>
+      <style jsx>{`
+        li.active span {
+          transform: none !important;
+          transform-origin: 50% 50% 0px;
+        }
+        li.active span.transition-transform {
+          transition: transform 0.3s ease-in-out;
+        }
+      `}</style>
     </div>
   )
 }
